3e8fdc858667f8520c7424a783a1daf926f2b52a
Medhansh Garg <medhansh2005@gmaill.com>
2025-07-04 12:20:05 +0530

    Cleaned up files | Old package is not longer supported by WorkFlowy API

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+/*
+ * Feature List:
+ * 1. ✅ Workflowy Authentication
+ * 2. ✅ Create Workflowy Node
+ * 3. Read Workflowy Node
+ * 4. Update Workflowy Node
+ * 5. Delete Workflowy Node
+ * 6. Complete Workflowy Node
+ */
+
+
 import fs from 'fs';
 import https from 'https';
-import Workflowy from './Workflowy.js';
 import dotenv from 'dotenv';
 dotenv.config();
+
+const CONFIG_PATH = './.wfconfig.json';
+const config = JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8'));
 
 const createNode = (title) => {
   const apiKey = process.env.WORKFLOWY_API_KEY;
@@ -51,96 +64,7 @@ const createNode = (title) => {
   req.end();
 };
 
-// let wf = new Workflowy({});
-// let email = "medhansh2005@gmail.com";
-// let schema = {
-//   properties: {
-//     email: {
-//       required: true
-//     }
-//   }
-// }
-
-// wf.getAuthType(email).then(authType => {
-  //   // if (authType == 'password') {
-//   //   schema = {properties: {
-//   //     password: {
-//   //       required: true,
-//   //       hidden: true
-//   //     }
-//   //   }}
-//   // } else if (authType == 'code') {
-  //   //   console.log(`An email has been sent to ${email} with a login code. Enter that code here:`)
-//   //   schema = {properties: {code: {required: true}}}
-//   // }
-
-//   // console.log(`Auth type for ${email} is ${authType}`);
-
-//   prompt.get(schema, function (err, result2) {
-  //     if (err) {console.log('\nCANCELLED\n'); process.exit(1)}
-//     wf = new Workflowy({username: email, password: result2.password, code: result2.code})
-//     wf.login().then(function () {
-  //       if (wf.sessionid) {
-    //         deferred.resolve("Successfully wrote sessionid to " + config_path)
-//       } else {
-//         deferred.reject(new Error("Failed to get sessionid"))
-//       }
-//     }).catch(err => {
-  //       console.log("err", err)
-  //     })
-  //   })
-  // }).catch(err => { console.log("err", err) });
 
 
-const CONFIG_PATH = './.wfconfig.json';
-function getSessionId() {
-  if (!fs.existsSync(CONFIG_PATH)) {
-    throw new Error(`⚠️ Config file not found at ${CONFIG_PATH}`);
-  }
-
-  const config = JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8'));
-
-  if (!config.sessionid) {
-    throw new Error('❌ sessionid not found in config');
-  }
-
-  return config.sessionid;
-}
-
-let wf = new Workflowy({sessionid: getSessionId()});
-let depth = 2;
-await wf.refresh();
-
-const test = wf.outline.then(function (outline) {
-  let rootnode = {
-    nm: 'root',
-    ch: outline,
-    id: ''
-  }
-  recursivePrint(rootnode, null, '', depth)
-}, err => { console.log("err", err) }).fin(() => process.exit())
-
-function recursivePrint (node, prefix, spaces, maxDepth) {
-  if (hiddencompleted && node.cp) {return}
-  if (!prefix) {prefix = '\u21b3 '}
-  if (!spaces) {spaces = ''}
-  let println = ''
-  println = spaces + prefix + node.nm
-  if (withnote && node.no) {
-    println += '\n'+spaces+'    '+ node.no
-  }
-
-  if (withid) {println += '\n[' + node.id + ']'}
-
-  console.log(println)
-
-  if (maxDepth < 1) {return}
-
-  let children = node.ch
-  for (let i in children) {
-    recursivePrint(children[i], prefix, spaces+' ', maxDepth-1)
-  }
-}
 
 // createNode("Test without node header");
-// workflowyPackageTest().catch(console.error);
